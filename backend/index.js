c57832faf77adea9c8429e3946bcc2126fbcdc29
“Timur” <senatorov.timur0@gmail.com>
2024-07-06 00:53:51 +0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello Express')
 })
 
+const apiRoute = require('./api');
+
+app.use('/api/', apiRoute);
+
 let port = 3001
 app.listen(port, () => {
     console.log(`Server on: http://localhost:${port}`)
